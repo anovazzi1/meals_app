5d094dd022833aeb7a20c9bb8db9c46addb17121
otavioanovazzi <otavio2204@gmail.com>
2022-03-29 22:03:49 -0300

    meals first screen set, not with right style

--- lib/categories_screen.dart
+++ lib/categories_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:meals_app/category.dart';
+import 'dummy_data.dart';
 
 class CategoriesScreen extends StatelessWidget {
   const CategoriesScreen({Key? key}) : super(key: key);
@@ -8,7 +10,7 @@ class CategoriesScreen extends StatelessWidget {
     return GridView(
       gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
           maxCrossAxisExtent: 200, childAspectRatio: 1.5,crossAxisSpacing: 20,mainAxisSpacing: 20),
-      children: [],
+      children: [...DUMMY_CATEGORIES.map((e) => CategoryItem(e.title, e.color)).toList()],
     );
   }
 }
